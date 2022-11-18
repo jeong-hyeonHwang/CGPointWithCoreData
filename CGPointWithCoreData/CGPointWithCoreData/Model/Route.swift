5fc86a6e9ae1284247b55b528e8f5589d15ea034
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-19 02:23:57 +0900

    [FEAT] Add Route Struct
    - Route Struct: For Making RouteFinding CoreData Record

---
+++ CGPointWithCoreData/CGPointWithCoreData/Model/Route.swift
@@ -0,0 +1,15 @@
+//
+//  Route.swift
+//  CGPointWithCoreData
+//
+//  Created by 황정현 on 2022/11/19.
+//
+
+import Foundation
+
+struct Route {
+    var dataWrittenDate: Date
+    var gymName: String
+    var problemLevel: Int
+    var isChallengeComplete: Bool
+}
