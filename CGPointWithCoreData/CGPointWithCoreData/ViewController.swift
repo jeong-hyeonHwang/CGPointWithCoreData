bac87462d267b0765560224bca7e28c87dfb21c8
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-18 15:41:26 +0900

    [FEAT] Add Initial Project

---
+++ CGPointWithCoreData/CGPointWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CGPointWithCoreData
+//
+//  Created by 황정현 on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
