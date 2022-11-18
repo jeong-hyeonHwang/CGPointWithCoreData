90eb4ca0b173cda85de173d4197f2c16f143435b
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-19 02:24:26 +0900

    [FEAT] Add Route Table View Cell

---
+++ CGPointWithCoreData/CGPointWithCoreData/Route/RouteTableViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  RouteTableViewCell.swift
+//  CGPointWithCoreData
+//
+//  Created by 황정현 on 2022/11/19.
+//
+
+import UIKit
+
+class RouteTableViewCell: UITableViewCell {
+
+    static let identifier = "RouteTableViewCell"
+    
+    lazy var writtenDateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    private lazy var gymNameLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var problemLevelLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var isChallengeCompleteLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        [writtenDateLabel, gymNameLabel, problemLevelLabel, isChallengeCompleteLabel].forEach({
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        NSLayoutConstraint.activate([
+            writtenDateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            writtenDateLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            writtenDateLabel.widthAnchor.constraint(equalToConstant: 300),
+            writtenDateLabel.heightAnchor.constraint(equalToConstant: 32)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been impl")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension RouteTableViewCell {
+    func labelConfigure(writtenDateText: String) {
+        writtenDateLabel.text = writtenDateText
+    }
+}
