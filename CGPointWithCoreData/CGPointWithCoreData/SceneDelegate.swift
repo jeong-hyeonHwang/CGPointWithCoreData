93db9e89a1078544fbf4fbf83396a310c6761dde
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-19 22:51:00 +0900

    [FIX] Revise RootViewController

--- CGPointWithCoreData/CGPointWithCoreData/SceneDelegate.swift
+++ CGPointWithCoreData/CGPointWithCoreData/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .white
-        window?.rootViewController = UINavigationController(rootViewController: RouteViewController())
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         window?.makeKeyAndVisible()
     }
 
