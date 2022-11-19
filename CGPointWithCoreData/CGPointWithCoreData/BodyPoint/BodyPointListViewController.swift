d69ce9ec0dc5feb3c3e2951150adaaff2a923456
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-20 06:47:04 +0900

    [FEAT] Add BodyPointListViewController
    - BodyPointListViewController: For Checking Page's BodyPoints number

---
+++ CGPointWithCoreData/CGPointWithCoreData/BodyPoint/BodyPointListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  BodyPointListViewController.swift
+//  CGPointWithCoreData
+//
+//  Created by 황정현 on 2022/11/20.
+//
+
+import UIKit
+
+class BodyPointListViewController: UIViewController {
+
+    var pageInfo: PageInfo!
+    
+    private lazy var bodyPointTableView = {
+        let view = UITableView()
+        
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layoutConfigure()
+        componentConfigure()
+        navigationBarConfigure()
+    }
+    
+    func layoutConfigure() {
+        [bodyPointTableView].forEach({
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        let safeArea = view.safeAreaLayoutGuide
+
+        let margin: CGFloat = 16
+        let buttonHeight: CGFloat = 50
+        
+        NSLayoutConstraint.activate([
+            bodyPointTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            bodyPointTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            bodyPointTableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            bodyPointTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -margin)
+        ])
+        
+
+    }
+    
+    func componentConfigure() {
+        
+        bodyPointTableView.delegate = self
+        bodyPointTableView.dataSource = self
+        bodyPointTableView.register(BodyPointListTableViewCell.self,
+                      forCellReuseIdentifier: BodyPointListTableViewCell.identifier)
+        
+        view.backgroundColor = .black
+        bodyPointTableView.backgroundColor = .black
+    }
+    
+    func navigationBarConfigure() {
+        self.navigationItem.title = "BODY POINT VC"
+    }
+}
+
+extension BodyPointListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pageInfo.points?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: BodyPointListTableViewCell.identifier, for: indexPath) as! BodyPointListTableViewCell
+        
+        let index = indexPath.row
+        cell.numberLabel.text = "POINT \(index)"
+        //        CoreDataManager.shared.readPageData(routeFinding: routeFinding)[index]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        print("CURRENT SELECTED POINT: \(index)")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+}
