a4539495543f29824a7b66d1948d4f0e276f28bf
jeonghyeon Hwang <hijihilin@gmail.com>
2022-11-20 06:43:57 +0900

    [FIX] Revise PageTableViewCell UI
    - Add TextColor to numberLabel
    - Add Cell Border Color & Width
    - Add Cell BackgroundColor
    - Add accessoryType

--- CGPointWithCoreData/CGPointWithCoreData/Page/PageTableViewCell.swift
+++ CGPointWithCoreData/CGPointWithCoreData/Page/PageTableViewCell.swift
@@ -15,6 +15,7 @@ class PageTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textAlignment = .left
         label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .yellow
         return label
     }()
     
@@ -32,12 +33,18 @@ class PageTableViewCell: UITableViewCell {
             numberLabel.widthAnchor.constraint(equalToConstant: 300),
             numberLabel.heightAnchor.constraint(equalToConstant: 32)
         ])
+        
+        self.layer.borderColor = UIColor.yellow.cgColor
+        self.layer.borderWidth = 2
+        self.backgroundColor = .clear
+        
+        self.accessoryType = .detailButton
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been impl")
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
